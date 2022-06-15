1da6a98331d66880923cc32b885d11ded6f07e30
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2022-06-15 13:43:40 -0400

    Tweaks for making xrandr work with new GPU configuration

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,6 +8,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bars
 
+sleep 1
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload bar1 &
