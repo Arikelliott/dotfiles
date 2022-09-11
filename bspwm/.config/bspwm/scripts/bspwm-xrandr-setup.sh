1d65e5b57f8076e1cf8f872fbe7606da06d22af9
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2022-09-11 14:20:09 -0400

    bspwm changes

---
+++ bspwm/.config/bspwm/scripts/bspwm-xrandr-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sleep 0.1
+xrandr --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-2 --primary --mode 2560x1440 --pos 1920x432 --rotate normal --output DP-3 --off --output DP-4 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-5 --off --output DP-6 --off --output DP-7 --off
