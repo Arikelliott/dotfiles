e2cd0ebe4b2b76c8e0b68d1874aca745b4131ff3
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-05-24 14:53:03 -0400

    Tweaks to zsh aliases

--- zsh/.config/zsh/plugins/custom/zsh-aliases.zsh
+++ zsh/.config/zsh/plugins/custom/zsh-aliases.zsh
@@ -4,7 +4,7 @@
 # General terminal aliases
 
 alias 'kitssh=kitty +kitten ssh'
-alias 'cl=clear'
+# alias 'cl=clear' # Just use ctrl + L
 alias 'cbonsai=cbonsai -li --time=0.5'
 alias 'cat-token=cat ~/repos/.personal-access-token1'
 alias 'gitstat=git status'
@@ -14,3 +14,16 @@ alias df='df -h'
 alias free='free -m'
 alias 'ls=ls -A --color=auto'
 alias 'll=ls -Alh --color=auto'
+alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
+alias ollama='docker exec -it ollama ollama'
+
+
+# yazi alias
+function ya() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
