d4a7f4a4b84398be239039f92c96e0b6b34136a8
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2024-05-24 15:53:02 -0400

    Changed zsh aliases to work with alternative utils such as exa, bat, etc

--- zsh/.config/zsh/plugins/custom/zsh-aliases.zsh
+++ zsh/.config/zsh/plugins/custom/zsh-aliases.zsh
@@ -12,11 +12,39 @@ alias 'snore=systemctl suspend'
 alias cp="cp -i"
 alias df='df -h'
 alias free='free -m'
-alias 'ls=ls -A --color=auto'
-alias 'll=ls -Alh --color=auto'
 alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 alias ollama='docker exec -it ollama ollama'
 
+# ls and eza aliases
+if command -v eza &> /dev/null
+then
+	alias 'ls=eza -A'
+	alias 'll=eza -Al'
+else
+	# alias 'ls=ls -A --color=auto'
+	# alias 'll=ls -Alh --color=auto'
+fi
+
+# cat and bat aliases
+if command -v bat &> /dev/null
+then
+	alias 'cat=bat --theme=base16'
+else
+fi
+
+# find and fd aliases
+if command -v fd &> /dev/null
+then
+	alias 'find=fd'
+else
+fi
+
+# find and fd aliases
+if command -v rg &> /dev/null
+then
+	alias 'grep=rg'
+else
+fi
 
 # yazi alias
 function ya() {
